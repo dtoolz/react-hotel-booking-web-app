feada6b839f2aae97fc63dcdd8a61e733e047a15
dtoolz <donaldaudriel@gmail.com>
2019-07-22 09:40:25 +0100

    added react routes and navigation

--- hotel/src/App.js
+++ hotel/src/App.js
@@ -4,16 +4,19 @@ import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
-
-
+import {Route, Switch} from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <Home/>
-      <Rooms/>
-      <SingleRoom/>
-      <Error/>
+     <Navbar/>
+     <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+     </Switch>
     </>
   );
 }
