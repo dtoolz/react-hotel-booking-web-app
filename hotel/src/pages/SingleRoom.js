15dcc2257a9ad7822747af0fcfb70e69cc2878c5
dtoolz <donaldaudriel@gmail.com>
2019-07-25 11:49:45 +0100

    added full details to single room page

--- hotel/src/pages/SingleRoom.js
+++ hotel/src/pages/SingleRoom.js
@@ -33,14 +33,51 @@ export default class SingleRoom extends Component {
            );
        }
        const {name,description,capacity,size,price,extras,breakfast,pets,images} = room;
+       //array of images destructuring to get main image of image[0] and the rest images
+       const [mainImg, ...defaultImg] = images;
         return (
-            <StyledHero img={images[0] || this.state.defaultBcg}>
+           <>
+            <StyledHero img={mainImg || this.state.defaultBcg}>
               <Banner title={`${name} room`} >
               <Link to='/rooms' className="btn-primary" >
                    Go Back To Rooms
                  </Link>
               </Banner>
             </StyledHero>
+            <section className="single-room" >
+              <div className="single-room-images" >
+                 {defaultImg.map((item,index) => {
+                    return ( <img key={index} src={item} alt={name} /> );
+                 })}
+              </div>
+              <div className="single-room-info" >
+                <article className="desc" >
+                   <h3>Details</h3>
+                   <p>{description}</p>
+                </article>
+                <article className="info" >
+                   <h3>info</h3>
+                   <h6>price : ${price}</h6>
+                   <h6>size : {size} SQFT</h6>
+                   <h6>
+                     max capacity : {capacity > 1 ? `${capacity} people` : `${capacity} person`}
+                   </h6>
+                   <h6>{pets ? "pets allowed" : "no pets allowed"}</h6>
+                   <h6>{breakfast && "Free Breakfast Available"}</h6>
+                </article>
+              </div>
+            </section>
+            <section className="room-extras">
+              <h6>Additional Benefits</h6>
+              <ul className="extras">
+                 {extras.map((item,index) =>{
+                   return (
+                     <li key={index}>-{item}</li>
+                   )
+                 })}
+              </ul>
+            </section>
+           </>
         )
     };
 };
